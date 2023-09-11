094d3685ade6b6a515b6b1fd10775818f795348f
Tabish Saleem <tabish.saleem@nxb.com.pk>
2023-09-11 16:43:06 +0500

    initail commit with products scaffold

---
+++ db/migrate/20230908170441_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :desription
+
+      t.timestamps
+    end
+  end
+end
